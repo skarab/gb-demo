4e1f48d93c9afc591bde4f308c70f9c57bacd761
Alexandre Chambriat <a.chambriat@gmail.com>
2021-06-27 09:56:24 +0200

    cleaning

--- backup/demo_old/make_resources.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call "C:\Program Files (x86)\sox-14-4-2\sox.exe" D:\prog\gameboy\samples\sega\Sega.wav -b 8 -c 1 --rate 8192 sega.raw
-call ..\tools\xxd\Release\xxd.exe -i -w sega.raw resources\sega_wav.h
-del sega.raw
-
-call ..\tools\xxd\Release\xxd.exe -i -w -x resources\title.raw resources\title.h
-call ..\tools\xxd\Release\xxd.exe -i -w -x resources\cube_header.raw resources\cube_header.h
